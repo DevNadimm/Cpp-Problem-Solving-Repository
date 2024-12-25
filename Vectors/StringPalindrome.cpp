3f41d51e6388a37e6f820156773763baa50fa79f
DevNadimm <nadimchowdhury87@gmail.com>
2024-12-25 22:14:03 +0600

    String Palindrome

---
+++ Vectors/StringPalindrome.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string reverseString(string input) {
+    int length = input.size();
+    int start = 0, end = length - 1;
+    while (start < end) {
+        swap(input[start], input[end]);
+        start++;
+        end--;
+    }
+    return input;
+}
+
+int main() {
+    vector<string> words = {"abc", "car", "ada", "racecar", "cool"};
+    vector<string> reversedWords;
+    vector<string> palindromes;
+
+    for (string word : words) {
+        reversedWords.push_back(reverseString(word));
+    }
+
+    for (int i = 0; i < words.size(); i++) {
+        if (words[i] == reversedWords[i]) {
+            string palindrome = reverseString(words[i]);
+            palindromes.push_back(palindrome);
+        }
+    }
+
+    for (string palindrome : palindromes) {
+        cout << palindrome << " ";
+    }
+
+    return 0;
+}
